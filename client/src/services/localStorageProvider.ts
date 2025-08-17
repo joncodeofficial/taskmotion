f4c5de5d559487c4a32446651aba2ddf8c1a2714
jonpena <gran_200014@hotmail.com>
2025-08-18 00:53:20 +0200

    refactor: remove function

--- client/src/services/localStorageProvider.ts
+++
@@ -1,8 +0,0 @@
-export const localStorageProvider = () => {
-  const map = new Map(JSON.parse(localStorage.getItem('taskmotion-cache') || '[]'));
-  window.addEventListener('beforeunload', () => {
-    const appCache = JSON.stringify(Array.from(map.entries()));
-    localStorage.setItem('taskmotion-cache', appCache);
-  });
-  return map;
-};
